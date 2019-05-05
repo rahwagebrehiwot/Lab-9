cbc61f8ae63ba96eab4ea8a1505b131d4f0e45f9
rahwagebrehiwot <47833393+rahwagebrehiwot@users.noreply.github.com>
2019-05-05 14:04:29 -0500

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var routes = require('./routes/routes.js')
-
-var app = express()
-app.use('/api', routes)
-
-var server = app.listen(process.env.PORT || 3000, function() {
-    console.log('app running on port', server.address().port)
-})
